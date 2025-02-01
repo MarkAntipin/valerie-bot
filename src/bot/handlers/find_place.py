3e386a839fd6e9a62dc8f175c35253f7886afb55
Эдгар Симонов <edgarsimonov@MacBook-Air.local>
2025-02-01 12:16:19 +0100

    limit user's message to less than 500 symbols

--- src/bot/handlers/find_place.py
+++ src/bot/handlers/find_place.py
@@ -12,13 +12,19 @@ logger = logging.getLogger(__name__)
 
 
 async def find_place_handler(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-    # TODO: limit input message length
+    max_length = 500
+    if len(update.message.text) > max_length:
+        await send_message(
+            message=update.message,
+            text=f"Your request is too long. Can you limit your message to {max_length} symbols, please"
+        )
+        return
+
     logger.info('user %s run find_place_handler', update.message.from_user.id)
 
     places_service = PlacesService(pg_pool=pg_pool)
 
     query: str = update.message.text
-
     res: str | None = await places_service.find_places(query=query)
 
     await send_message(
